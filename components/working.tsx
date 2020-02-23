f45332107c99c82b23bd5e25da6d601884f81978
Maxi Gimenez <gimenez.maxi@gmail.com>
2020-02-23 14:48:10 +0100

    chore: typo

--- components/working.tsx
+++ components/working.tsx
@@ -4,7 +4,7 @@ const Working = () => {
       <div className="working">
         <h2>Working</h2>
         <p>Currently, I'm at <a href="https://ganintegrity.com" title="GAN Integrity" target="_blank">GAN Integrity</a> as Senior Front-end engineer in the 🇩🇰 Copenhagen offices.</p>
-        <p>My daily job is to develop high performing code using the latest technologies aiming to have full covered code (TDD & E2E) for the SaaS product.</p>
+        <p>My daily job is to develop high performing code using the latest technologies aiming to have it fully covered (TDD & E2E) for the SaaS product.</p>
       </div>
 
       <style jsx>{`
