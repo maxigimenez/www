9c8725fc4f89026e8653f48f233260618950c09a
Maxi Gimenez <gimenez.maxi@gmail.com>
2020-05-22 14:49:37 +0200

    chore: lot of improvements

--- core/contentful.ts
+++ core/contentful.ts
@@ -16,44 +16,66 @@ export class ContentFulService {
   });
 
   getPosts(): Promise<Post[]> {
-    return new Promise(async resolve => {
-      const { items } = await this._client.getEntries({
-        'content_type': ContentType.BLOG_POST
-      });
-
-      resolve(items.map((post: any) => {
-        return {
-          title: post.fields.title,
-          image: post.fields.image.fields.file.url,
-          body: post.fields.body,
-          introBody: post.fields.introBody,
-          imageAlt: post.fields.image.fields.description,
-          slug: post.fields.slug
-        };
-      }));
+    return new Promise(async (resolve, reject) => {
+      try {
+        const { items } = await this._client.getEntries({
+          'content_type': ContentType.BLOG_POST
+        });
+        resolve(items.map(post => _createPost(post)));
+      } catch (e) {
+        reject(e);
+      }
     });
   }
 
   getPostBySlug(slug: string): Promise<Post> {
     return new Promise(async (resolve, reject) => {
-      const { items }: any = await this._client.getEntries({
-        'content_type': ContentType.BLOG_POST,
-        'fields.slug[in]': slug
-      });
+      try {
+        const { items }: any = await this._client.getEntries({
+          'content_type': ContentType.BLOG_POST,
+          'fields.slug[in]': slug
+        });
 
-      const [post] = items;
-      if (!post) {
-        return reject();
+        const [post] = items;
+        if (!post) {
+          return reject();
+        }
+
+        resolve(_createPost(post));
+      } catch (e) {
+        reject(e);
       }
+    });
+  }
 
-      resolve({
-        title: post.fields.title,
-        image: post.fields.image.fields.file.url,
-        body: post.fields.body,
-        introBody: post.fields.introBody,
-        imageAlt: post.fields.image.fields.description,
-        slug: post.fields.slug
-      });
+  getPostsSlugs(): Promise<string[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const { items } = await this._client.getEntries({
+          'content_type': ContentType.BLOG_POST,
+          select: 'fields.slug'
+        });
+        resolve(items.map((post: any) => post.fields.slug));
+      } catch (e) {
+        reject(e);
+      }
     });
   }
 }
+
+const _createPost = post => ({
+  title: post.fields.title,
+  image: post.fields.image?.fields?.file?.url || '',
+  body: post.fields.body || '',
+  introBody: post.fields.introBody || '',
+  imageAlt: post.fields.image?.fields?.description || '',
+  slug: post.fields.slug || '',
+  createdAt: _formatDate(post.fields.createdAt || '')
+});
+
+const _formatDate = (date: string) => {
+  const options = { year: 'numeric', month: 'short', day: 'numeric' };
+  return new Intl.DateTimeFormat('en-US', options).format(
+    new Date(date)
+  );
+};
