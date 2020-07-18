5f4f8505b1c75e3cc92855afa7b8d1c48ac5f274
Maxi Gimenez <gimenez.maxi@gmail.com>
2020-07-18 11:05:29 +0200

    chore: catch error

--- scripts/generate-readme.js
+++ scripts/generate-readme.js
@@ -18,22 +18,30 @@ const getPosts = async () => {
     host: process.env.NODE_ENV !== 'production' ? 'preview.contentful.com' : 'cdn.contentful.com'
   });
 
-  const { items } = await client.getEntries({
-    'content_type': 'post'
-  });
+  try {
+    const { items } = await client.getEntries({
+      'content_type': 'post'
+    });
 
-  DATA.posts = items.map(post => ({
-    title: post.fields.title,
-    url: `https://maxigimenez.xyz/post/${post.fields.slug}`
-  }));
+    DATA.posts = items.map(post => ({
+      title: post.fields.title,
+      url: `https://maxigimenez.xyz/post/${post.fields.slug}`
+    }));
+  } catch (e) {
+    throw e;
+  }
 };
 
 const getInstagramPhotos = async () => {
-  const response = await fetch('https://www.instagram.com/gmaxi_/?__a=1');
-  const json = await response.json();
-  DATA.photos = json.graphql.user.edge_owner_to_timeline_media.edges.splice(0, 3).map(({node}) => ({
-    url: node.display_url
-  }));
+  try {
+    const response = await fetch('https://instagram.com/gmaxi_/?__a=1');
+    const json = await response.json();
+    DATA.photos = json.graphql.user.edge_owner_to_timeline_media.edges.splice(0, 3).map(({node}) => ({
+      url: node.display_url
+    }));
+  } catch (e) {
+    throw e;
+  }
 };
 
 const generate = () => {
