291ac12de9e457a85a552a69a639e8d389c6708b
Maxi <gimenez.maxi@gmail.com>
2026-02-04 15:41:31 +0000

    chore: revert changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,16 +5,16 @@ export default defineNuxtConfig({
 
   devtools: { enabled: true },
 
-  app: {
-    head: {
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap',
-        },
-      ],
-    },
-  },
+  // app: {
+  //   head: {
+  //     link: [
+  //       {
+  //         rel: 'stylesheet',
+  //         href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap',
+  //       },
+  //     ],
+  //   },
+  // },
 
   modules: ['@nuxt/content', '@nuxt/icon'],
 
